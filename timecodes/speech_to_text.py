47a622ec90a886a2d7f2f04e63f4ef05c9e6f545
MulhamShaheen <mulhamshaheen.ms@gmail.com>
2024-05-20 00:42:00 +0300

    Add short segments filtering

--- timecodes/speech_to_text.py
+++ timecodes/speech_to_text.py
@@ -5,6 +5,7 @@ import gc
 
 from dotenv import load_dotenv
 
+
 load_dotenv("../.env")
 
 HF_TOKEN = os.getenv("HF_TOKEN")
@@ -20,14 +21,18 @@ class SpeechToTextWhisperX:
                  model_name: str = "tiny"):
         self.batch_size = batch_size
         self.compute_type = compute_type
-        self.model = whisperx.load_model("tiny", device, compute_type=self.compute_type,
+        self.model = whisperx.load_model(model_name, device, compute_type=self.compute_type,
                                          download_root="models/whisperx/")
 
-    def __call__(self, audio_file: str, save_path: str = "result.txt"):
+    def __call__(self, audio_file: str, save_path: str = "result.txt", filter_short_segments: bool = True):
         audio = whisperx.load_audio(audio_file)
-        result = self.model.transcribe(audio, batch_size=self.batch_size)
-        model_a, metadata = whisperx.load_align_model(language_code=result["language"], device=device)
+        result = self.model.transcribe(audio, batch_size=self.batch_size, language="ru")
+        model_a, metadata = whisperx.load_align_model(language_code="ru", device=device)
         result = whisperx.align(result["segments"], model_a, metadata, audio, device, return_char_alignments=False)
+
+        if filter_short_segments:
+            result["segments"] = [seg for seg in result["segments"] if seg["end"] - seg["start"] > 2 and len(seg["text"].split(" ")) > 5]
+
         with open("result.txt", "w", encoding="utf-8") as f:
             for seg in result["segments"]:
                 f.write(f"{seg['start']} {seg['end']} {seg['text']}\n")
