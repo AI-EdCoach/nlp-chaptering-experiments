02e5fcebdd1c84367b2c813d636044c16ec0395f
MulhamShaheen <mulhamshaheen.ms@gmail.com>
2024-05-20 00:41:26 +0300

    Tune threshold in ChapterTimecodesPipeline

--- timecodes/speech_to_chapters.py
+++ timecodes/speech_to_chapters.py
@@ -11,22 +11,25 @@ from transformers.pipelines import pipeline
 
 class ChapterEstimator:
     def __init__(self, model_checkpoint: str = 'paraphrase-multilingual-MiniLM-L12-v2',
-                 hf_model: str = "bert-base-multilingual-cased"):
+                 hf_model: str = "bert-base-multilingual-cased", threshold: float | None = None):
         self.model = SentenceTransformer(model_checkpoint)
         self.hf_model = pipeline("feature-extraction", model=hf_model)
+        self.threshold = threshold
 
-    def __call__(self, text: Union[str, List[str]], threshold: float = None) -> pd.DataFrame:
+    def __call__(self, text: Union[str, List[str]], **kwargs) -> pd.DataFrame:
         if isinstance(text, List):
             sentences = text
         else:
             sentences = self.get_sentences(text)
+
         embeddings = self.get_embeddings(sentences)
         dimension = len(embeddings[0])
         index = faiss.IndexFlatL2(dimension)
         index.add(np.array(embeddings).astype('float16'))
 
         distances = []
-        values_dict = {"index": [i for i in range(len(sentences))], "sentence": sentences, "embedding": [e for e in embeddings]}
+        values_dict = {"index": [i for i in range(len(sentences))], "sentence": sentences,
+                       "embedding": [e for e in embeddings]}
         df = pd.DataFrame(values_dict)
         for i in range(len(embeddings) - 1):
             distance = index.search(np.array([embeddings[i]]).astype('float32'), 2)[0][0][1]
@@ -34,16 +37,18 @@ class ChapterEstimator:
 
         df['faiss_search'] = distances + [None]
         cos_dist = []
-        for i in range(len(embeddings) - 1):
-            sim = cosine_similarity(embeddings[i].reshape(1, -1), embeddings[1 + 1].reshape(1, -1))[0][0]
+        for i in range(1, len(embeddings)):
+            sim = cosine_similarity(embeddings[1-i].reshape(1, -1), embeddings[i].reshape(1, -1))[0][0]
             cos_dist.append(sim)
 
         df['cos_sim'] = cos_dist + [None]
         df['super_score'] = df['faiss_search'] * df['cos_sim']
+        threshold = self.threshold
+
         if threshold is None:
-            threshold = df['super_score'].quantile(.25)
+            threshold = df['super_score'].quantile(.10)
         df['chapter'] = [1 if score < threshold else 0 for score in df['super_score']]
-        return df
+        return df[['index', 'sentence', 'super_score', 'chapter']]
 
     def get_embeddings(self, sentences: List[str]):
         embeddings = self.model.encode(sentences)
