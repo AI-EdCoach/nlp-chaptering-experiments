866122459271cfb6dc543f82a238e3e55195ee3e
Andrey <ashilkov2000@mail.ru>
2024-06-20 21:09:44 +0500

    added sign up and login pages

---
+++ frontend/pages/login.py
@@ -0,0 +1,113 @@
+import dash
+from http import HTTPStatus
+from requests import Response
+from dash import Dash, html, dcc, Input, Output, callback, State
+from dash.exceptions import PreventUpdate
+import dash_bootstrap_components as dbc
+from typing import List, Dict, Tuple
+from pprint import pprint
+from frontend.api_utils import login_via_api, get_current_user_data_via_api
+
+
+dash.register_page(__name__)
+
+
+@callback(
+    Output("session", "data"),
+    Output("login_successful", "is_open"),
+    Output("login_missing-data", "is_open"),
+    Output("login_error", "is_open"),
+    Input("login_button", "n_clicks"),
+    State("session", "data"),
+    State("login_form_email", "value"),
+    State("login_form_password", "value"),
+)
+def login_user(
+    n_clicks: int, session_data: Dict, login_email: str, login_password: str
+) -> Tuple[Dict, bool, bool, bool]:
+    if n_clicks is None:
+        raise PreventUpdate
+    login_successful = False
+    login_missing_data = False
+    login_error = False
+    if n_clicks == 0:
+        return session_data, login_successful, login_missing_data, login_error
+    if None in [login_email, login_password]:
+        login_missing_data = True
+        return session_data, login_successful, login_missing_data, login_error
+    response: Response = login_via_api(email=login_email, password=login_password)
+    if response.status_code == HTTPStatus.OK:
+        token = response.json()["access_token"]
+        session_data["token"] = token
+        cur_user_response = get_current_user_data_via_api(access_token=token)
+        session_data["current_user"] = cur_user_response.json()
+        login_successful = True
+        return session_data, login_successful, login_missing_data, login_error
+
+    login_error = True
+    return session_data, login_successful, login_missing_data, login_error
+
+
+def get_login_form():
+    form = dbc.Form(
+        dbc.Row(
+            [
+                dbc.Label("Email", width="auto"),
+                dbc.Col(
+                    dbc.Input(
+                        type="email", id="login_form_email", placeholder="Enter email"
+                    ),
+                    className="me-3",
+                ),
+                dbc.Label("Password", width="auto"),
+                dbc.Col(
+                    dbc.Input(
+                        type="password",
+                        id="login_form_password",
+                        placeholder="Enter password",
+                    ),
+                    className="me-3",
+                ),
+                dbc.Col(
+                    dbc.Button("Login", id="login_button", color="primary", n_clicks=0),
+                    width="auto",
+                ),
+            ],
+            className="g-2",
+        )
+    )
+    return form
+
+
+def layout():
+    form = get_login_form()
+    layout = html.Div(
+        [
+            dbc.Alert(
+                "Successfully logged in",
+                color="success",
+                id="login_successful",
+                is_open=False,
+                duration=1000,
+            ),
+            dbc.Alert(
+                "Please fill the sign up form",
+                color="warning",
+                id="login_missing-data",
+                is_open=False,
+                duration=1000,
+            ),
+            dbc.Alert(
+                "Could not log in with these credentials",
+                color="danger",
+                id="login_error",
+                is_open=False,
+                duration=1000,
+            ),
+            html.H5("Login", style={"margin-top": "5%", "margin-left": "5%"}),
+            html.Div(
+                children=[form], style={"margin-right": "5%", "margin-left": "5%"}
+            ),
+        ]
+    )
+    return layout
---
+++ frontend/pages/sign_up.py
@@ -0,0 +1,117 @@
+import dash
+from http import HTTPStatus
+from requests import Response
+from dash import Dash, html, dcc, callback, Input, Output, State
+from dash.exceptions import PreventUpdate
+import dash_bootstrap_components as dbc
+from frontend.api_utils import register_user_via_api
+from typing import Dict, List, Tuple
+
+dash.register_page(__name__)
+
+
+@callback(
+    Output("register-successful", "is_open"),
+    Output("register_missing-data", "is_open"),
+    Output("register_error", "is_open"),
+    Input("signup-button", "n_clicks"),
+    # form data
+    State("register_form_username", "value"),
+    State("register_form_email", "value"),
+    State("register_form_pwd", "value"),
+)
+def register_user(
+    n_clicks: int, user_name: str, user_email: str, user_password: str
+) -> Tuple[bool, bool, bool]:
+    if n_clicks is None:
+        raise PreventUpdate
+    register_successful = False
+    register_missing_data = False
+    register_error = False
+    if n_clicks == 0:
+        return register_successful, register_missing_data, register_error
+    if None in [user_email, user_password]:
+        register_missing_data = True
+        return register_successful, register_missing_data, register_error
+    print(user_name, user_email, user_password)
+    print(user_name, user_email, user_password)
+    response: Response = register_user_via_api(
+        username=user_name, email=user_email, password=user_password
+    )
+    if response.status_code == HTTPStatus.OK:
+        register_successful = True
+        return register_successful, register_missing_data, register_error
+    register_error = True
+    return register_successful, register_missing_data, register_error
+
+
+def get_signup_form():
+    email_input = html.Div(
+        [
+            dbc.Input(
+                type="email", id="register_form_email", placeholder="Enter email"
+            ),
+        ],
+        className="mb-3",
+    )
+
+    username_input = html.Div(
+        [
+            dbc.Input(
+                type="text", id="register_form_username", placeholder="Enter username"
+            ),
+        ],
+        className="mb-3",
+    )
+
+    password_input = html.Div(
+        [
+            dbc.Input(
+                type="password",
+                id="register_form_pwd",
+                placeholder="Enter password",
+            ),
+        ],
+        className="mb-3",
+    )
+    signup_button = dbc.Button(
+        "Sign Up", id="signup-button", color="primary", n_clicks=0
+    )
+    sign_up_form = dbc.Form(
+        [email_input, username_input, password_input, signup_button],
+        style={"margin-right": "5%", "margin-left": "5%"},
+    )
+    return sign_up_form
+
+
+def layout():
+    sign_up_form = get_signup_form()
+    layout = html.Div(
+        [
+            dbc.Alert(
+                "Successfully registered",
+                color="success",
+                id="register-successful",
+                is_open=False,
+                duration=1000,
+            ),
+            dbc.Alert(
+                "Please fill the sign up form",
+                color="warning",
+                id="register_missing-data",
+                is_open=False,
+                duration=1000,
+            ),
+            dbc.Alert(
+                "Could not register",
+                color="danger",
+                id="register_error",
+                is_open=False,
+                duration=1000,
+            ),
+            html.H5("Sign Up", style={"margin-top": "5%", "margin-left": "5%"}),
+            html.Div(children=[sign_up_form]),
+            html.Span(id="hidden-signup-span", style={"display": "none"}),
+        ]
+    )
+    return layout
